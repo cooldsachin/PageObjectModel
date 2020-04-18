dabb0b74864c31cf150aea56ea585b542f7d2c48
cooldsachin <sachindeshmukh463@gmail.com>
2020-04-17 21:46:46 -0500

    Update FreeCrmTest.java

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -20,7 +20,7 @@ public class FreeCrmTest {
 
 	@BeforeMethod
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "/OnPremiseDevOps/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/OnPremiseDevOps/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		js = (JavascriptExecutor) driver;
 		driver.get("https://www.freecrm.com/index.html");
